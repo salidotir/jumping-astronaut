5d0ede5e3c11312dd116fe3a108c02b316d0d209
Sara Limooee <salidotir@gmail.com>
2021-08-21 11:22:56 +0430

    add jumping to astronaut

---
+++ Assets/Scripts/TapController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour {
+
+    public float tapForce = 250;
+    public float tiltSmooth = 2;
+    public Vector3 stopPosition;
+
+    Rigidbody2D rigidBody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+    public GameObject startPage;
+    public GameObject gameOverPage;
+    public Text scoreText;
+    int score = 0;
+    public bool gameOver = false;
+    enum PageState
+    {
+        None,
+        Start,
+        GameOver
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        downRotation = Quaternion.Euler(0, 0, -90);
+        forwardRotation = Quaternion.Euler(0, 0, 35);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))    // left-click on PC OR tap on mobiles
+        {
+            transform.rotation = forwardRotation;
+
+            rigidBody.velocity = Vector3.zero;
+            rigidBody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+        }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+
+    void OnTrigger2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "ScoreZone")
+        {
+            // get a score
+            score++;
+            scoreText.text = score.ToString();
+
+            // play a sound
+
+        }
+
+        if (col.gameObject.tag == "DeadZone")
+        {
+            rigidBody.simulated = false;           // freeze the astronaut
+
+            // game over
+            gameOver = true;
+            int savedScore = PlayerPrefs.GetInt("HighScore");
+            if (score > savedScore)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+            setPageState(PageState.GameOver);
+
+
+            // play a sound
+
+        }
+    }
+
+    void setPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(false);
+                break;
+
+            case PageState.Start:
+                startPage.SetActive(true);
+                gameOverPage.SetActive(false);
+                break;
+
+            case PageState.GameOver:
+                startPage.SetActive(false);
+                gameOverPage.SetActive(true);
+                break;
+        }
+    }
+
+
+}
